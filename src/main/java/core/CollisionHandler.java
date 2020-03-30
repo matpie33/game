c2d15c279e81894e0df5094b9666b76c0ee0f5ea
matpie33 <Spaaw@poczta.fm>
2020-03-30 20:24:58 +0200

    Replace magic numbers in collision handler. Fix clearing boxes from view.

--- src/main/java/core/CollisionHandler.java
+++ src/main/java/core/CollisionHandler.java
@@ -3,6 +3,7 @@ package core;
 import com.jme3.asset.AssetManager;
 import com.jme3.bullet.collision.PhysicsCollisionEvent;
 import com.jme3.bullet.collision.PhysicsCollisionListener;
+import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.effect.ParticleEmitter;
 import com.jme3.effect.ParticleMesh;
 import com.jme3.material.Material;
@@ -11,9 +12,25 @@ import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
+import constants.PhysicsControls;
 
 public class CollisionHandler implements PhysicsCollisionListener {
 
+	public static final int MINIMUM_IMPULSE_TO_DESTROY_BOX = 9;
+	public static final float STARTING_SIZE = 1.5f;
+	public static final float ENDING_SIZE = 0.4f;
+	public static final int NUMBER_OF_TIMES_TO_REPEAT = 0;
+	public static final float GRAVITY_FORCE = 12f;
+	public static final float MINIMUM_ANIMATION_TIME = 3f;
+	public static final float MAXIMUM_ANIMATION_TIME = 4f;
+	public static final int STARTING_VELOCITY_Y = 15;
+	public static final float VELOCITY_VARIATION = .60f;
+	public static final int IMAGES_IN_TEXTURE_HORIZONTALLY = 3;
+	public static final int IMAGES_IN_TEXTURE_VERTICALLY = 3;
+	public static final int NUMBER_OF_PIECES = 15;
+	public static final String MATERIAL_DEFINITION_PATH = "Common/MatDefs/Misc/Particle.j3md";
+	public static final String TEXTURE_PATH = "models/debris.png";
+	public static final String TEXTURE_NAME = "Texture";
 	private ModelLoader modelLoader;
 	private AssetManager assetManager;
 	private Node rootNode;
@@ -37,47 +54,51 @@ public class CollisionHandler implements PhysicsCollisionListener {
 									.contains(nodeA);
 		boolean isBBox = modelLoader.getBoxes()
 									.contains(nodeB);
-		if ((isABox || isBBox) && event.getAppliedImpulse() > 9) {
-			if (isABox && nodeA.getParent() != null) {
-				nodeA.getParent()
-					 .detachChild(nodeA);
-			}
-			if (isBBox && nodeB.getParent() != null) {
-				nodeB.getParent()
-					 .detachChild(nodeB);
-			}
+		if ((isABox || isBBox) && event.getAppliedImpulse() > MINIMUM_IMPULSE_TO_DESTROY_BOX) {
+			clearNode(nodeA, isABox);
+			clearNode(nodeB, isBBox);
 
 			ParticleEmitter boxParticles = new ParticleEmitter("Box",
-					ParticleMesh.Type.Triangle, 15);
+					ParticleMesh.Type.Triangle, NUMBER_OF_PIECES);
 			boxParticles.setSelectRandomImage(true);
 			boxParticles.setRandomAngle(true);
 			boxParticles.setRotateSpeed(FastMath.TWO_PI * 4);
 
 			boxParticles.setStartColor(new ColorRGBA(ColorRGBA.Brown));
-			boxParticles.setStartSize(1.5f);
-			boxParticles.setEndSize(0.4f);
+			boxParticles.setStartSize(STARTING_SIZE);
+			boxParticles.setEndSize(ENDING_SIZE);
 
-			boxParticles.setParticlesPerSec(0);
-			boxParticles.setGravity(0, 12f, 0);
-			boxParticles.setLowLife(4.4f);
-			boxParticles.setHighLife(4.5f);
+			boxParticles.setParticlesPerSec(NUMBER_OF_TIMES_TO_REPEAT);
+			boxParticles.setGravity(0, GRAVITY_FORCE, 0);
+			boxParticles.setLowLife(MINIMUM_ANIMATION_TIME);
+			boxParticles.setHighLife(MAXIMUM_ANIMATION_TIME);
 			boxParticles.getParticleInfluencer()
-						.setInitialVelocity(new Vector3f(0, 15, 0));
+						.setInitialVelocity(new Vector3f(0, STARTING_VELOCITY_Y, 0));
 			boxParticles.getParticleInfluencer()
-						.setVelocityVariation(.60f);
-			boxParticles.setImagesX(3);
-			boxParticles.setImagesY(3);
-			Material mat = new Material(assetManager,
-					"Common/MatDefs/Misc/Particle.j3md");
-			mat.setTexture("Texture",
-					assetManager.loadTexture("models/debris.png"));
-			boxParticles.setMaterial(mat);
+						.setVelocityVariation(VELOCITY_VARIATION);
+			boxParticles.setImagesX(IMAGES_IN_TEXTURE_HORIZONTALLY);
+			boxParticles.setImagesY(IMAGES_IN_TEXTURE_VERTICALLY);
+			Material material = new Material(assetManager,
+					MATERIAL_DEFINITION_PATH);
+			material.setTexture(TEXTURE_NAME,
+					assetManager.loadTexture(TEXTURE_PATH));
+			boxParticles.setMaterial(material);
 			boxParticles.setLocalTranslation(nodeA.getLocalTranslation());
 			boxParticles.emitAllParticles();
 			rootNode.attachChild(boxParticles);
 
 		}
 
+	}
+
+	private void clearNode(Spatial node, boolean isBox) {
+		if (isBox && node.getParent() != null) {
+			RigidBodyControl control = node.getControl(
+					PhysicsControls.BOX);
+			control.getPhysicsSpace().remove(control);
+			node.getParent()
+				 .detachChild(node);
+		}
 	}
 
 }
