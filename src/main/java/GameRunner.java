985ac77ebe5c68f6f967cf96168f5d9671912cfe
matpie33 <Spaaw@poczta.fm>
2020-04-04 13:19:05 +0200

    Set resolution according to screen size.

--- src/main/java/GameRunner.java
+++ src/main/java/GameRunner.java
@@ -1,11 +1,16 @@
 import com.jme3.system.AppSettings;
 import core.GameApplication;
 
+import java.awt.*;
+
 public class GameRunner {
 
 	public static void main(String[] args) {
 		GameApplication gameApplication = GameApplication.getInstance();
 		AppSettings settings = new AppSettings(true);
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		Dimension screenSize = tk.getScreenSize();
+		settings.setResolution(screenSize.width, screenSize.height);
 		settings.setFullscreen(true);
 		gameApplication.setShowSettings(false);
 		gameApplication.setSettings(settings);
